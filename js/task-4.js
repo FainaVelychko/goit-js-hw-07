eddb89cc9e335abfe3ca10b9571a6b723fd5a34f
FainaVelychko <fainarw@gmail.com>
2024-03-10 11:29:55 +0200

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector('.login-form');
 
+const handleSubmit = event => {
+  event.preventDefault();
+  const form = event.target;
+  const userEmail = form.elements.email.value;
+  const userPassword = form.elements.password.value;
+
+  if (userEmail === '' || userPassword === '') {
+    alert('All form fields must be filled in');
+  }
+  const userData = {
+    email: userEmail.trim(),
+    password: userPassword.trim(),
+  };
+
+  console.log(userData);
+  form.reset();
+};
+
+formEl.addEventListener('submit', handleSubmit);
