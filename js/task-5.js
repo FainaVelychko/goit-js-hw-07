e0db9648f26be599e78d37e09fa399338a730c55
FainaVelychko <fainarw@gmail.com>
2024-03-10 12:37:44 +0200

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('button.change-color');
+const body = document.querySelector('body');
+const spanText = document.querySelector('span.color');
+const handleClick = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  spanText.textContent = `${getRandomHexColor()}`;
+};
+
+button.addEventListener('click', handleClick);
